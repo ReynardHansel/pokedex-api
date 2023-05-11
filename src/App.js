756688f48c9b4b11065b6e65ddf480feba83cabf
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2023-05-11 12:54:54 +0700

    Finished basic pokedex
    - Displaying names
    - Pagination
    - CSS not included

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
+import PokemonList from './PokemonList'
+import axios from 'axios'
+import Pagination from './Pagination'
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true) // loads everytime page changes (url changes)
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    })
+      .then(res => {
+      setLoading(false)
+      setPrevPageUrl(res.data.previous)
+      setNextPageUrl(res.data.next)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+
+    return () => cancel()
+  }, [currentPageUrl]) // --> Every time the currentPageUrl changes, rerun the whole code in the useEffect
+
+  function nextPage(){
+    setCurrentPageUrl(nextPageUrl)
+  }
+  function prevPage(){
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  if(loading) return "Loading..."
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <h1>Hi</h1> */}
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        nextPage={nextPageUrl ? nextPage : null}
+        prevPage={prevPageUrl ? prevPage : null}
+      />
+    </>
   );
 }
 
